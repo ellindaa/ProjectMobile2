ce08e913f568faf8755796521239171ae3dfb2ed
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-11-21 16:55:02 +0700

    Update comment

--- app/src/main/java/id/ellinda/newsapp/DetailActivity.kt
+++ app/src/main/java/id/ellinda/newsapp/DetailActivity.kt
@@ -8,27 +8,33 @@ import id.ellinda.newsapp.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
+    // variabel binding dideklarasikan dengan 'lateinit' yaitu akan diinisialisasi nanti
 
     companion object {
         const val cont_TitleNews = "cont_TitleNews"
         const val cont_PhotoNews = "cont_PhotoNews"
         const val cont_KontenNews = "cont_KontenNews"
     }
+    // companion object -> untuk mendeklarasikan konstanta yang dapat diakses tanpa membuat instance dari kelas
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding.root) 
 
         supportActionBar?.hide()
+        // menyembunyikan action bar
 
+        // mengambil data dari intent
+        // intent adalah cara untuk berkomunikasi dan berinteraksi antar bagian dalam aplikasi android
         binding.txtTitleNews.text = intent.getStringExtra(cont_TitleNews) ?: "No Title"
         binding.txtKontenNews.text = intent.getStringExtra(cont_KontenNews) ?: "No Content"
-
+        // menampilkan gambar 
         intent.getStringExtra(cont_PhotoNews)?.toIntOrNull()?.let {
             binding.imgToolbar.setImageResource(it)
         }
-
+        // tombol kembali
         binding.btnBack.setOnClickListener {
             val toMain = Intent(this@DetailActivity, MainActivity::class.java)
             startActivity(toMain)
