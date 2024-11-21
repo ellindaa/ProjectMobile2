4b92ea9527d1d229562de5c40c694509dd737d10
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-11-21 17:09:29 +0700

    Update comment

--- app/src/main/java/id/ellinda/newsapp/BeritaAdapter/NewsAdapter.kt
+++ app/src/main/java/id/ellinda/newsapp/BeritaAdapter/NewsAdapter.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import id.ellinda.newsapp.R
-import id.ellinda.newsapp.BeritaAdapter.news // Pastikan ini sesuai dengan struktur Anda
-import id.ellinda.newsapp.databinding.LayBeritaBinding // Pastikan ini sesuai dengan nama file layout Anda
+import id.ellinda.newsapp.BeritaAdapter.news 
+import id.ellinda.newsapp.databinding.LayBeritaBinding 
 
+// konstruktor kelas NewsAdapter menerima 2 parameter (context, dan list)
+// NewsAdapter mewarisi dari RecyclerView.Adapter
+// MyViewHolder sebagai kelas ViewHolder
 class NewsAdapter(private val context: Context, private val list: List<news>) : RecyclerView.Adapter<NewsAdapter.MyViewHolder>() {
 
     private lateinit var onItemClickCallBack: OnItemClickCallback
@@ -28,20 +31,21 @@ class NewsAdapter(private val context: Context, private val list: List<news>) : 
         val binding = LayBeritaBinding.inflate(LayoutInflater.from(context), parent, false)
         return MyViewHolder(binding)
     }
-
+    // Metode onBindViewHolder -> mengikat data berita ke ViewHolder pada posisi tertentu d
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val dataNews = list[position]
         holder.setData(dataNews)
     }
-
+    // Metode setOnClickCallback -> untuk menetapkan callback ketika item berita di klik
     fun setOnClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallBack = onItemClickCallback
     }
-
+    // Interface -> untuk menangani klik pada item berita
     interface OnItemClickCallback {
         fun onItemClick(data: news)
     }
 
+    // getItemCount -> mengembalikan jumlah item dalam daftar berita
     override fun getItemCount(): Int {
         return list.size
     }
