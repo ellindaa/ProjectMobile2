9ff7bf85528e6e98d24c83218485681f8bb852b4
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 18:57:29 +0700

    update

--- app/src/main/java/id/ellinda/amphibians/data/AmphibiansRepository.kt
+++ app/src/main/java/id/ellinda/amphibians/data/AmphibiansRepository.kt
@@ -1,38 +1,18 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.ellinda.amphibians.data
 
 import id.ellinda.amphibians.model.Amphibian
 import id.ellinda.amphibians.network.AmphibiansApiService
 
-/**
- * Repository retrieves amphibian data from underlying data source.
- */
+// Repositori mengambil data amfibi dari sumber data 
 interface AmphibiansRepository {
-    /** Retrieves list of amphibians from underlying data source */
+    // Mengambil daftar amfibi dari sumber data 
     suspend fun getAmphibians(): List<Amphibian>
 }
 
-/**
- * Network Implementation of repository that retrieves amphibian data from underlying data source.
- */
+ // Implementasi Jaringan repositori yang mengambil data amfibi dari sumber data
 class DefaultAmphibiansRepository(
     private val amphibiansApiService: AmphibiansApiService
 ) : AmphibiansRepository {
-    /** Retrieves list of amphibians from underlying data source */
+    // Mengambil daftar amfibi dari sumber data 
     override suspend fun getAmphibians(): List<Amphibian> = amphibiansApiService.getAmphibians()
 }
