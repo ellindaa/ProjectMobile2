97b18f0aec4f64a8891afeb42a42be2a97a37fe6
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 16:29:45 +0700

    Update

--- app/src/main/java/id/ellinda/marsphotos/model/MarsPhoto.kt
+++ app/src/main/java/id/ellinda/marsphotos/model/MarsPhoto.kt
@@ -1,30 +1,15 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.ellinda.marsphotos.model
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-/**
- * This data class defines a Mars photo which includes an ID, and the image URL.
- */
+// anotasi untuk mengubah data menjadi format yang bisa dikirim ke server atau disimpan, dan bisa mengubahnya kembali menjadi objek yang bisa digunakan di aplikasi.
 @Serializable
+// Data Class MarsPhoto
 data class MarsPhoto(
     val id: String,
+    // petunjuk untuk memberi tahu bahwa saat mengubah data ini menjadi format lain (JSON), kita akan menggunakan nama img_src untuk properti ini.
     @SerialName(value = "img_src")
+    // tempat untuk menyimpan URL gambar dari foto mars 
     val imgSrc: String
 )
