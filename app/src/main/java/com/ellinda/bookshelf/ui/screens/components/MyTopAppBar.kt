a3397ee1dd8d6618c1bb6d48dc57bf6075bfee19
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 20:25:37 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/ui/screens/components/MyTopAppBar.kt
+++ app/src/main/java/com/ellinda/bookshelf/ui/screens/components/MyTopAppBar.kt
@@ -11,12 +11,13 @@ import com.ellinda.bookshelf.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopAppBar(
-    currentScreen: AppDestinations,
-    canNavigateBack: Boolean,
-    onNavigateUpClicked: () -> Unit
+    currentScreen: AppDestinations, // menyimpan info tentang layar saat ini
+    canNavigateBack: Boolean, // menunjukkan apakah user dpt kembali ke layar sebelumnya, jika true ikon panah kembali akan ditampilkan
+    onNavigateUpClicked: () -> Unit // sebuah lambda yang akan dipanggil ketika ikon navigasi kembali ditekan
 ) {
     TopAppBar(
-        title = { Text(text = currentScreen.title) },
+        // bilah aplikasi di bagian atas layar
+        title = { Text(text = currentScreen.title) }, // judul layar saat ini
         navigationIcon = {
             if (canNavigateBack){
                 IconButton(
