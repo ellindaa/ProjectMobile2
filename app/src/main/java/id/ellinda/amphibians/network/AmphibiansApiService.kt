ac3287ac0721a9a0f2736f883568e95a8d8993ef
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 19:12:21 +0700

    update

--- app/src/main/java/id/ellinda/amphibians/network/AmphibiansApiService.kt
+++ app/src/main/java/id/ellinda/amphibians/network/AmphibiansApiService.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.ellinda.amphibians.network
 
 import id.ellinda.amphibians.model.Amphibian
@@ -21,5 +5,7 @@ import retrofit2.http.GET
 
 interface AmphibiansApiService {
     @GET("amphibians")
+    // metode getAmphibians melakukan permintaan GET ke endpoint
+    // ketika metode ini dipanggil, Retrofit akan mengirimkan permintaan HTTP GET ke URL
     suspend fun getAmphibians(): List<Amphibian>
 }
