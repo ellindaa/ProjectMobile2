dffd7486113c705aecf7587c1af304c312ce8981
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 19:45:46 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/di/AppContainer.kt
+++ app/src/main/java/com/ellinda/bookshelf/di/AppContainer.kt
@@ -3,10 +3,11 @@ package com.ellinda.bookshelf.di
 import com.ellinda.bookshelf.data.BookshelfRepository
 import com.ellinda.bookshelf.network.BookshelfApiService
 
-/**
- * Dependency Injection container at the application level.
- */
+// wadah dependencu injection pada tingkat aplikasi
 interface AppContainer {
+    // mengembalikan instance dari BookshelfApiService, memungkinkan bagian lain dari apl utk mengakses layanan API
+    // tanpa perlu mengetauhi bagaimana layanan tsb diinisialisasi
     val bookshelfApiService: BookshelfApiService
+    // mengembalikan instance dari BookshelfRepository, memungkinkan bagian lain dari apl utk mengakses repository yg mengelola pengambilan data buku
     val bookshelfRepository: BookshelfRepository
 }
