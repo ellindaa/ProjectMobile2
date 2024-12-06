7e78c76bb63d106daa4dbbc6677b109d6b7a19ff
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 20:14:18 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/network/BookshelfApiService.kt
+++ app/src/main/java/com/ellinda/bookshelf/network/BookshelfApiService.kt
@@ -11,19 +11,19 @@ import retrofit2.http.Query
  * A public interface that exposes the [getBooks] method
  */
 interface BookshelfApiService {
-
     companion object {
+        // objek pendamping (companion object) yang berisi konstanta
         const val BASE_URL = "https://www.googleapis.com/books/v1/"
+        // URL untuk membangun permintaan ke endpoint yang sesuai
     }
 
-    /**
-     * Returns a [List] of [Book] and this method can be called from a Coroutine.
-     * The @GET annotation indicates that the "volumes" endpoint will be requested with the GET
-     * HTTP method
-     */
+    // Mengembalikan [List] dari [Book] dan metode ini dapat dipanggil dari Coroutine.
+    // Anotasi @GET menunjukkan bahwa titik akhir "volume" akan diminta dengan GET
     @GET("volumes")
     suspend fun getBooks(@Query("q") query: String): Response<QueryResponse>
+    // mengambil daftar buku berdasarkan query pencarian yang diberikan
 
     @GET("volumes/{id}")
     suspend fun getBook(@Path("id") id: String): Response<Book>
+    // mengambil detail dari buku tertentu berdasarkan id buku
 }
