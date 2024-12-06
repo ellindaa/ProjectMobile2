37ef9bb153e6628104def0e621cfbce5d01d2398
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 20:08:35 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/model/QueryResponse.kt
+++ app/src/main/java/com/ellinda/bookshelf/model/QueryResponse.kt
@@ -4,8 +4,9 @@ import kotlinx.serialization.Serializable
 
 // Notes: the null here is KEY, we need it, in case nothing is found
 @Serializable
+// kelas data QueryResponse merepresentasikan respons dari kueri yang dilakukan ke API
 data class QueryResponse(
-    val items: List<Book>?,
-    val totalItems: Int,
-    val kind: String,
+    val items: List<Book>?, // menyimpan daftar buku yang ditemukan
+    val totalItems: Int, // memberikan informasi jumlah total buku
+    val kind: String, // memberikan konteks tambahan tentang jenis respons yg diterima
 )
