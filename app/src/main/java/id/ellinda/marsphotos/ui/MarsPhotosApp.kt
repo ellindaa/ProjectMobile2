ffeda20fe1fc2ac380c588bb3ea165333e53b8fa
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 18:42:04 +0700

    Update

--- app/src/main/java/id/ellinda/marsphotos/ui/MarsPhotosApp.kt
+++ app/src/main/java/id/ellinda/marsphotos/ui/MarsPhotosApp.kt
@@ -1,20 +1,5 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 @file:OptIn(ExperimentalMaterial3Api::class)
+// anotasi ini digunakan untuk menunjukkan bahwa kita menggunakan API eksperimental dari Material3
 
 package id.ellinda.marsphotos.ui
 
@@ -40,16 +25,22 @@ import id.ellinda.marsphotos.ui.screens.MarsViewModel
 @Composable
 fun MarsPhotosApp() {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    // mengatur perilaku gulir dari TopAppBar
     Scaffold(
+        // komponen yang menyediakan struktur dasar untuk layout, termasuk area untuk app bar, dan konten.
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { MarsTopAppBar(scrollBehavior = scrollBehavior) }
     ) {
         Surface(
+            // komponen yang memberikan latar belakang dan mengatur ukuran konten didalamnya
             modifier = Modifier.fillMaxSize()
         ) {
             val marsViewModel: MarsViewModel =
                 viewModel(factory = MarsViewModel.Factory)
+                // mengambil instance dari MarsViewMoodel menggunakan viewModel(factory = MarsViewModel.Factory)
+                // untuk mengelola dan menyimpan UI state
             HomeScreen(
+                // memanggil fungsi komposabel HomeScreen dan mengoper statue UI marsUiState
                 marsUiState = marsViewModel.marsUiState,
                 retryAction = marsViewModel::getMarsPhotos,
                 contentPadding = it
@@ -60,7 +51,9 @@ fun MarsPhotosApp() {
 
 @Composable
 fun MarsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    // fungsi untuk membangun app bar yang terletak di bagian atas aplikasi
     CenterAlignedTopAppBar(
+        // judul terletak pada bagian tengah
         scrollBehavior = scrollBehavior,
         title = {
             Text(
