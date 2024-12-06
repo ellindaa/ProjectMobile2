0eee07e4988f2729df572f42959f23189e601d6b
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 20:19:00 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/ui/screens/components/ErrorScreen.kt
+++ app/src/main/java/com/ellinda/bookshelf/ui/screens/components/ErrorScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,24 +21,29 @@ import com.ellinda.bookshelf.ui.theme.BookshelfTheme
 @Composable
 fun ErrorScreen(
     retryAction: () -> Unit,
+    // sebuah lambda yang akan dipanggil ketika user menekan tombol coba lagi
     modifier: Modifier = Modifier
 ) {
     Column(
+        // menyusun elemen UI secara vertikal
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
+            // menampilkan gambar yg menunjukkan kesalahan koneksi
             modifier = Modifier.fillMaxWidth(),
             painter = painterResource(id = R.drawable.ic_connection_error),
             contentDescription = null,
             contentScale = ContentScale.Fit
         )
         Text(
+            // menampilkan pesan kesalahan yang diambil dari string
             stringResource(R.string.failed_to_load_msg),
             fontWeight = FontWeight.Bold
         )
         Button(onClick = retryAction) {
+            // tombol untuk mencoba lagi
             Text(stringResource(R.string.btn_try_again))
         }
     }
