80b88605771f4f01e61dbc15b7fc63a9e5146f16
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 19:17:52 +0700

    update

--- app/src/main/java/id/ellinda/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/id/ellinda/amphibians/ui/screens/HomeScreen.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.ellinda.amphibians.ui.screens
 
 import androidx.compose.foundation.Image
@@ -73,9 +57,7 @@ fun HomeScreen(
     }
 }
 
-/**
- * The home screen displaying the loading message.
- */
+ // home screen menampilkan pesan pemuatan
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
     Image(
@@ -85,9 +67,7 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
     )
 }
 
-/**
- * The home screen displaying error message with re-attempt button.
- */
+ // home screen menampilkan pesan kesalahan dengan tombol coba ulang
 @Composable
 fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
     Column(
