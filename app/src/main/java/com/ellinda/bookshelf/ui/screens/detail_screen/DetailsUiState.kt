c3b71328ff0988eadc0bc8a52fcc9332069b2fef
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 20:42:12 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/ui/screens/detail_screen/DetailsUiState.kt
+++ app/src/main/java/com/ellinda/bookshelf/ui/screens/detail_screen/DetailsUiState.kt
@@ -2,8 +2,12 @@ package com.ellinda.bookshelf.ui.screens.detail_screen
 
 import com.ellinda.bookshelf.model.Book
 
+// sebuah sealed interface untuk mendefinisikan hierarki tipe yang terbatas
 sealed interface DetailsUiState {
+    // data class yang mewakili status sukses ketika detail buku berhasil dimuat
     data class Success(val bookItem: Book) : DetailsUiState
+    // objek singleton yang mewakili status kesalahan, ketika terjadi kesalahan dlm memuat detail buku status ini akan di gunakan
     object Error : DetailsUiState
+    // data sedang dimual, status ini digunakan untuk menampilkan indikator loading di UI
     object Loading : DetailsUiState
 }
