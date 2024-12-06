ea395c027801af8c7d92ee24c201c2639bf610af
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 16:35:03 +0700

    Update

--- app/src/main/java/id/ellinda/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/id/ellinda/marsphotos/network/MarsApiService.kt
@@ -1,33 +1,13 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.ellinda.marsphotos.network
 
 import id.ellinda.marsphotos.model.MarsPhoto
 import retrofit2.http.GET
 
-/**
- * A public interface that exposes the [getPhotos] method
- */
+// method getPhotos akan mengembalikan daftar foto Mars dan dapat dipanggil dari coroutine
 interface MarsApiService {
-    /**
-     * Returns a [List] of [MarsPhoto] and this method can be called from a Coroutine.
-     * The @GET annotation indicates that the "photos" endpoint will be requested with the GET
-     * HTTP method
-     */
+    // memberi tahu retrofit bahwa kita meminta data dari URL yang berisi photos
+    // ketika memanggil metode ini, retrofit akan mengirimkan permintaan ke server untuk mendapatkan foto-foto Mars.
     @GET("photos")
+    // fungsi yang akan dijalankan tanpa mengganggu proses lainnya
     suspend fun getPhotos(): List<MarsPhoto>
 }
