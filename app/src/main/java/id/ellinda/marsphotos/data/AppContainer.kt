5001467644bfd228322fe33a6ace9a0712a0f6b3
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 16:08:55 +0700

    Update

--- app/src/main/java/id/ellinda/marsphotos/data/AppContainer.kt
+++ app/src/main/java/id/ellinda/marsphotos/data/AppContainer.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package id.ellinda.marsphotos.data
 
 import id.ellinda.marsphotos.network.MarsApiService
@@ -21,39 +6,32 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 
-/**
- * Dependency Injection container at the application level.
- */
+// perjanjian bahwa setiap kelas yang mengimplementasikan AppContainer harus memiliki marsPhotosRepository
+// membantu untuk mengatur dan mengelola berbagai bagian dari apliakasi, khususnya yang berhubungan dengan data.
 interface AppContainer {
     val marsPhotosRepository: MarsPhotosRepository
 }
 
-/**
- * Implementation for the Dependency Injection container at the application level.
- *
- * Variables are initialized lazily and the same instance is shared across the whole app.
- */
+
 class DefaultAppContainer : AppContainer {
+    // alamat dasar dari API yang digunakan 
     private val baseUrl = "https://android-kotlin-fun-mars-server.appspot.com/"
 
-    /**
-     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
-     */
+    // mengatur retrofit (alat untuk berkomunikasi dengan API)
     private val retrofit: Retrofit = Retrofit.Builder()
+    // mengatur cara mengubah data dari format JSON menjadi objek yang dapat digunakan pada aplikasi
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl)
         .build()
 
-    /**
-     * Retrofit service object for creating api calls
-     */
+    // rerofitService adalah objek yang akan digunakan untuk berkomunikasi dengan API
+    // by lazy, objek hanya akan dibuat saat dibutuhkan 
     private val retrofitService: MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
     }
 
-    /**
-     * DI implementation for Mars photos repository
-     */
+    // marsPhotoRepository, tempat mengambil data dari API
+    // menggunakan NetworkMarsPhotosRepository yang memanfaatkan retrofitService untuk mengambil data dari API
     override val marsPhotosRepository: MarsPhotosRepository by lazy {
         NetworkMarsPhotosRepository(retrofitService)
     }
