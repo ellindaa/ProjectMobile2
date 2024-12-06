8142ab8e3e019f9e947d1511de26f9b56355ca69
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 18:45:33 +0700

    Update

--- app/src/main/java/id/ellinda/marsphotos/MainActivity.kt
+++ app/src/main/java/id/ellinda/marsphotos/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package id.ellinda.marsphotos
 
 import android.os.Bundle
@@ -29,13 +13,16 @@ import id.ellinda.marsphotos.ui.theme.MarsPhotosTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
+        // metode onCreate dipanggil saat aktivitas dibuat
         super.onCreate(savedInstanceState)
+        // memanggil metode onCreate dari kelas induk untuk memastikan bahwa semua inisialisasi yang diperlukan dilakukan
         setContent {
             MarsPhotosTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
                     MarsPhotosApp()
+                    // memanggil fungsi komposabel yang merupakan fungsi utama untuk membangun UI user aplikasi
                 }
             }
         }
