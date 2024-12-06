39dc29dd3dcc8fc58653978e626f3d80a0c34f26
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 19:51:52 +0700

    update

--- app/src/main/java/com/ellinda/bookshelf/di/DefaultAppContainer.kt
+++ app/src/main/java/com/ellinda/bookshelf/di/DefaultAppContainer.kt
@@ -7,19 +7,26 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 
+// kelas implementasi dari interface AppContainer yang bertanggung jawab untuk menyediakan instance dari  BookshelfApiService dan  BookshelfRepository
 class DefaultAppContainer : AppContainer {
 //    private val json = Json {
 //        ignoreUnknownKeys = true
 //        explicitNulls = false
 //    }
     override val bookshelfApiService: BookshelfApiService by lazy {
+        // by lazy, properti ini akan diinisialisasi saat pertama kali di akses
         Retrofit.Builder()
+        // membuat builder untuk retrofit
             .addConverterFactory(GsonConverterFactory.create())
+            // menambahkan converter utk mengkonversi JSON ke objek kotlin menggunakan Gson
 //            .addConverterFactory(json
 //                    .asConverterFactory("application/json".toMediaType()))
             .baseUrl(BookshelfApiService.BASE_URL)
+            // menetapkan URL dasar untuk API
             .build()
+            // membangun instance Retrofit
             .create()
+            // membuat instance dari BookshelfApiService
     }
 
     override val bookshelfRepository: BookshelfRepository by lazy {
