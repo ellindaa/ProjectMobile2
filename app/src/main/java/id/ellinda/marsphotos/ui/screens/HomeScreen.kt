674248e9db7517c9c35e706315452b0951e66517
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-06 16:47:56 +0700

    Update

--- app/src/main/java/id/ellinda/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/id/ellinda/marsphotos/ui/screens/HomeScreen.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package id.ellinda.marsphotos.ui.screens
 
 import androidx.compose.foundation.Image
@@ -49,25 +34,30 @@ import id.ellinda.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
+    // menyimpan status UI (Loading, Success, Error)
     marsUiState: MarsUiState,
+    // fungsi yang akan dipanggil jika user ingin mencoba lagi
     retryAction: () -> Unit,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
+    // logika when digunakan untuk memeriksa status marsUiState dan menampilkan layar yang sesuai
     when (marsUiState) {
+        // menampilkan LoadingScreen
         is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        // menampilkan PhotosGridScreen dengan daftar foto
         is MarsUiState.Success -> PhotosGridScreen(
             marsUiState.photos, contentPadding = contentPadding, modifier = modifier.fillMaxWidth()
         )
+        // menampilkan ErrorScreen dengan opsi untuk mencoba lagi
         is MarsUiState.Error -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
     }
 }
 
-/**
- * The home screen displaying the loading message.
- */
+
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
+    // menampilkan gambar loading saat data sedang dimuat
     Image(
         modifier = modifier.size(200.dp),
         painter = painterResource(R.drawable.loading_img),
@@ -75,29 +65,29 @@ fun LoadingScreen(modifier: Modifier = Modifier) {
     )
 }
 
-/**
- * The home screen displaying error message with re-attempt button.
- */
+
 @Composable
 fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
     Column(
+        // menggunakan Column untuk menyusun elemen secara vertikal
         modifier = modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        // menampilkan gambar kesalahan
         Image(
             painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
         )
+        // teks yang menjelaskan kesalahan
         Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        // tombol untuk mencoba lagi
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
         }
     }
 }
 
-/**
- * The home screen displaying photo grid.
- */
+
 @Composable
 fun PhotosGridScreen(
     photos: List<MarsPhoto>,
@@ -105,10 +95,12 @@ fun PhotosGridScreen(
     contentPadding: PaddingValues = PaddingValues(0.dp),
 ) {
     LazyVerticalGrid(
+        // menampilkan daftar foto dalam bentuk grid yang dapat di gulir secara vertikal
         columns = GridCells.Adaptive(150.dp),
         modifier = modifier.padding(horizontal = 4.dp),
         contentPadding = contentPadding,
     ) {
+        // mengiterasi daftar foto, dan untuk setiap foto memanggil MarsPhotoCard untuk menampilkan foto tersebut
         items(items = photos, key = { photo -> photo.id }) { photo ->
             MarsPhotoCard(
                 photo,
@@ -123,11 +115,14 @@ fun PhotosGridScreen(
 
 @Composable
 fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
+    // menampilkan foto dalam bentuk kartu dengan efek elevasi atau bayangan
     Card(
         modifier = modifier,
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
+        // digunakan untuk membuat gambar secara asinkron menggunakan Coil
+        // Coil, sebuah pustaka untuk memuat gambar di Android
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current).data(photo.imgSrc)
                 .crossfade(true).build(),
