109516ef97e2cdcefb4e3cf58754fdced7b166a1
ellindaa <147477519+ellindaa@users.noreply.github.com>
2024-12-26 21:57:07 +0700

    initial update

--- app/src/main/java/id/ellinda/myapplication/navigation/NavHost.kt
+++ app/src/main/java/id/ellinda/myapplication/navigation/NavHost.kt
@@ -10,24 +10,30 @@ import androidx.navigation.navArgument
 @Composable
 fun Navigation(){
     val navController = rememberNavController()
+
+    // TODO: menentukan host navigasi dg rute awal Banner Screen
     NavHost(navController = navController, startDestination = "Banner screen") {
+        // TODO: menambahkan rute untuk layar Banner Screen
         composable("Banner screen") {
             BannerScreen(navController= navController)
         }
+        // TODO: menambahkan rute untuk layar Home Screen
         composable("Home screen"){
             HomeScreen(navController= navController)
         }
+        // TODO: menambahkan rute untuk layar Details Screen
         composable("Details screen/{id}",
             arguments = listOf(
                 navArgument(
-                    name = "id"
+                    name = "id" 
                 ) {
                     type = NavType.IntType
                 }
             )
         ) {id->
+            // TODO: mendapatkan nilai id dan memanggil detail screen
             id.arguments?.getInt("id")?.let { id1->
-                DetailsScreen(id =id1)
+                DetailsScreen(id =id1) // TODO: menampilkan layar detail dengan id yang sesuai
             }
 
         }
